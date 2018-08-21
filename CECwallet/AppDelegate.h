d6b9612b583789c42636ef52662de90050445ee7
通证控股 <tzkg@tongzhengkonggudemacbook-pro.local>
2018-08-21 14:58:59 +0800

    Initial Commit

---
+++ CECwallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CECwallet
+//
+//  Created by 通证控股 on 2018/8/21.
+//  Copyright © 2018年 通证控股. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CECwallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CECwallet
+//
+//  Created by 通证控股 on 2018/8/21.
+//  Copyright © 2018年 通证控股. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
